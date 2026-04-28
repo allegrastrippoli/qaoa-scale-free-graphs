e328ca886a7f6eccb6328fc376f88e3453438f5e
Allegra Strippoli <allegra@strippoli.eu>
2026-04-28 14:31:32 -0400

    add min energy to df

--- src/optimization/optimizedangles.py
+++ src/optimization/optimizedangles.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 class OptimizedAngles:
-    def __init__(self, df=None):
+    def __init__(self, df=None, algo=None):
         self.df = df
         
     def compute(self, graphs, p, algo_name, history_filename=None, initial_angles=None, multistart_iter=0):
@@ -13,10 +13,10 @@ class OptimizedAngles:
             algo = AlgorithmFactory.create(algo=algo_name, G=G, p=p)
             algo.run(multistart_iter=multistart_iter, initial_angles=initial_angles)
             gamma, beta = algo.angles
-            data.append([i, gamma, beta])
+            data.append([i, gamma, beta, algo.energy])
             if hasattr(algo, "history") and algo.history:
                 history_to_csv(algo_name=algo_name, best_bitstring=algo.best_bitstring, history=algo.history, filename=history_filename)
-        self.df = pd.DataFrame(data, columns=["graph_id", "gamma", "beta"])
+        self.df = pd.DataFrame(data, columns=["graph_id", "gamma", "beta", "energy"])
             
     def save(self, filename):
          self.df.to_csv(filename, index=False)
@@ -28,3 +28,6 @@ class OptimizedAngles:
          gammas = self.df["gamma"].to_numpy()
          betas = self.df["beta"].to_numpy()
          return gammas, betas
+     
+    def get_min_energies(self):
+        return self.df["energy"].to_numpy()
