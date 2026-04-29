48eeee71b4ea073569f9b7ff03908ce928a2edec
Allegra Strippoli <allegra@strippoli.eu>
2026-04-29 14:36:11 -0400

    validate inputs

--- src/algorithms/algofactory.py
+++ src/algorithms/algofactory.py
@@ -23,19 +23,36 @@ class AlgorithmFactory:
     
 @AlgorithmFactory.register("qaoa")
 def build_qaoa(G, p, **kwargs):
+    _validate_inputs(G, p)
     H = graph_to_hamiltonian(nx.to_numpy_array(G), len(G.nodes))
     return QAOA(p=p, H=H)
 
 @AlgorithmFactory.register("rqaoa")
 def build_rqaoa(G, p, **kwargs):
+    _validate_inputs(G, p)
     H = graph_to_hamiltonian(nx.to_numpy_array(G), len(G.nodes))
     Q = QAOA(p=p, H=H)
     return RecursiveQAOA(p=p, H=H, Q=Q, G=G)
 
 @AlgorithmFactory.register("lcqaoa")
 def build_lcqaoa(G, p, **kwargs):
+    _validate_inputs(G, p)
     return LightConesQAOA(G=G, p=p, **kwargs)
 
 @AlgorithmFactory.register("aqaoa")
 def build_lcqaoa(G, p, **kwargs):
+    _validate_inputs(G, p)
     return AnalyticalQAOA(G=G, p=p, **kwargs)
+
+def _validate_inputs(G, p):
+    if not isinstance(G, nx.Graph):
+        raise TypeError("G must be a networkx.Graph")
+
+    if G.number_of_nodes() < 2:
+        raise ValueError("G must have at least two nodes")
+
+    if G.number_of_edges() < 1:
+        raise ValueError("G must have at least one edge")
+
+    if not isinstance(p, int) or p <= 0:
+        raise ValueError("p must be a positive integer")
